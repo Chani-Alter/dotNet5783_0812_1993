761252d2c58dfd609bb8dd58031e392b23463cd4
c0548544858@gmail.com <c0548544858@gmail.com>
2022-10-26 13:28:07 +0300

    עשיתי את השינויים שהיה צריך לעשות בפרק 6 של תרגיל 0

---
+++ dotNet5783_0812_1993/Stage0/Program1993.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Stage0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome0812();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome1993();
+        private static void Welcome0812()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application");
+        }
+    }
+}
