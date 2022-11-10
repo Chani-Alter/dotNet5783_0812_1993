d0aa6f03b8e424d96d0fb4b1475cf74ce87d5832
Adi Bialostozky <shalev.arc@gmail.com>
2022-11-10 09:08:56 +0200

    מימוש המחלקה dalOrder

--- dotNet5783_0812_1993/DalList/DalOrder.cs
+++ dotNet5783_0812_1993/DalList/DalOrder.cs
@@ -1,4 +1,5 @@
 ﻿using DO;
+using System.Drawing;
 using static Dal.DataSource;
 
 namespace Dal;
@@ -7,9 +8,50 @@ public class DalOrder
 {
     public int Add(Order order)
     {
-        int id = DataSource.Config.OrderItemId;
+        int id = Config.OrderItemId;
         order.ID= id;
-        DataSource.OrderArray[DataSource.Config.IndexOrderArray++]= order;
+        OrderArray[Config.IndexOrderArray++]= order;
         return id;
     }
+    public Order GetById(int id)
+    {
+        for(int i = 0; i < OrderArray.Length; i++)
+        {
+           if( OrderArray[i].ID == id)
+                return OrderArray[i];
+        }
+        throw new Exception("Order is not exist");
+    }
+    public Order[] GetAll()
+    {
+        Order[] orders = new Order[OrderArray.Length];  
+        for (int i = 0; i < OrderArray.Length; i++)
+        {
+            orders[i] = OrderArray[i];
+        }
+            return orders;
+    }
+
+    public void Delete(int id)
+    {
+        int i;
+        for ( i = 0; i < OrderArray.Length || OrderArray[i].ID != id; i++) ;
+        if(i== OrderArray.Length)
+            throw new Exception("order is not exist");
+        i++;
+        for (; i < OrderArray.Length; i++)
+            OrderArray[i - 1] = OrderArray[i];
+    }
+
+    public void Update(Order order)
+    {
+        int i;
+        for (i = 0; i < OrderArray.Length || OrderArray[i].ID != order.ID; i++) ;
+        if (i == OrderArray.Length)
+            throw new Exception("order is not exist");
+        OrderArray[i]= order;
+    }
+
+
+
 }
