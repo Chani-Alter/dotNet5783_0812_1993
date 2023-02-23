e6a0cd9f2db9488aaa8b80a0718034db95bf3c44
c0548544858@gmail.com <c0548544858@gmail.com>
2023-02-23 16:09:38 +0200

    עיצוב

--- dotNet5783_0812_1993/DalXml/DalProduct.cs
+++ dotNet5783_0812_1993/DalXml/DalProduct.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Dal
-{
-    internal class DalProduct
-    {
-    }
-}
+//namespace Dal
+//{
+//    internal class DalProduct
+//    {
+//    }
+//}
--- dotNet5783_0812_1993/DalXml/DalXml.cs
+++ dotNet5783_0812_1993/DalXml/DalXml.cs
@@ -1,42 +1,42 @@
-﻿using DalApi;
-namespace Dal;
+﻿//using DalApi;
+//namespace Dal;
 
-/// <summary>
-/// a class how containes all the dal classes
-/// </summary>
-sealed internal class DalXml : IDal
-{
-    #region PUBLIC MEMBERS
-    public static IDal? Instance
-    {
-        get
-        {
-            if (instance == null)
-            {
-                lock (key)
-                {
-                    if (instance == null)
-                        instance = new DalXml();
-                }
-            }
-            return instance;
-        }
-    }
+///// <summary>
+///// a class how containes all the dal classes
+///// </summary>
+//sealed internal class DalXml : IDal
+//{
+//    #region PUBLIC MEMBERS
+//    public static IDal? Instance
+//    {
+//        get
+//        {
+//            if (instance == null)
+//            {
+//                lock (key)
+//                {
+//                    if (instance == null)
+//                        instance = new DalXml();
+//                }
+//            }
+//            return instance;
+//        }
+//    }
 
-    public IOrder Order { get; } = new DalOrder();
-    public IProduct Product { get; } = new DalProduct();
-    public IOrderItem OrderItem { get; } = new DalOrderItem();
+//    public IOrder Order { get; } = new DalOrder();
+//    public IProduct Product { get; } = new DalProduct();
+//    public IOrderItem OrderItem { get; } = new DalOrderItem();
 
-    #endregion
+//    #endregion
 
-    #region PRIVATE MEMBERS
+//    #region PRIVATE MEMBERS
 
-    private static readonly object key = new();
+//    private static readonly object key = new();
 
-    private static IDal? instance;
+//    private static IDal? instance;
 
-    private DalXml() { }
+//    private DalXml() { }
 
-    #endregion
-}
+//    #endregion
+//}
 
